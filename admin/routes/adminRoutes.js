e2b0a6e7b13bbb34b6a525b08214c90b4fc5c6de
Chanakarn Kingkaew <chanakarnkin@pim.ac.th>
2022-11-25 12:57:56 +0700

    Backup

--- admin/routes/adminRoutes.js
+++ admin/routes/adminRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const database = require("../../config/database")
 const jwt = require("jsonwebtoken")
-const {authenticateToken} = require("../../config/jwt");
+const { authenticateToken } = require("../../config/jwt");
 
 router.post("/login", (req, res) => {
   const username = req.body.username;
@@ -12,7 +12,7 @@ router.post("/login", (req, res) => {
                   where Username = "${username}"`, (err, rows) => {
     if (rows.length > 0) {
       if (rows[0].Password === password) {
-        const token = jwt.sign({id: rows[0].No}, process.env.JWT_SECRET);
+        const token = jwt.sign({ id: rows[0].No }, process.env.JWT_SECRET);
         return res.send(token);
       }
       return res.send(400)
@@ -24,7 +24,7 @@ router.post("/login", (req, res) => {
 router.get("/list", authenticateToken, (req, res) => {
   const q = req.query.q;
   const by = req.query.by;
-  if (q) {
+  if (q & by) {
     database.query(`select *
                     from adminsystem
                     where ${by} LIKE "%${q}%"`, (err, rows) => {
@@ -32,6 +32,23 @@ router.get("/list", authenticateToken, (req, res) => {
     })
   } else {
     database.query("select * from adminsystem", (err, rows) => {
+      res.send(rows)
+    })
+  }
+})
+
+router.get("/song/list/:type", authenticateToken, (req, res) => {
+  const type = req.params.type
+  const q = req.query.q;
+  const by = req.query.by;
+  if (q & by) {
+    database.query(`select *
+                    from ${type}
+                    where ${by} LIKE "%${q}%"`, (err, rows) => {
+      res.send(rows)
+    })
+  } else {
+    database.query(`select * from ${type}`, (err, rows) => {
       res.send(rows)
     })
   }
